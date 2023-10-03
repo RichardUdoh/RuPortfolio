83816fe62ed68d1314eeca2cfab7d2d774e18664
Richard Udoh <udohrichard201114@gmail.com>
2023-10-03 18:06:39 +0200

    transition done

--- components/Transition.js
+++ components/Transition.js
@@ -1,9 +1,47 @@
+import {motion} from 'framer-motion'
+
+const transitionVariants = {
+  initial: {
+    x: '100%',
+    width: '100%' 
+  },
+  animate: { 
+    x: '0%',
+    width: '0%' 
+
+  },
+  exit:{ 
+    x: ['0%', '100%'],
+    width: ['0%', '100%']
+  },
+}
+
 const Transition = () => {
   return (
     <>
-      <div>Transition</div>
+      <motion.div className='fixed top-0 bottom-0 right-full h-screen z-30 w-screen bg-[#2e2257]' 
+      variants={transitionVariants} 
+      initial='initial' 
+      animate='animate' exit='exit'
+      transition={{delay:0.1, duration:0.4, ease:'easeInOut' }}
+      >
+      </motion.div>
+      <motion.div className='fixed top-0 bottom-0 right-full h-screen z-20 w-screen bg-[#3b2d71]' 
+      variants={transitionVariants} 
+      initial='initial' 
+      animate='animate' exit='exit'
+      transition={{delay:0.3, duration:0.4, ease:'easeInOut' }}
+      >
+      </motion.div>
+      <motion.div className='fixed top-0 bottom-0 right-full h-screen z-10 w-screen bg-[#4b3772]' 
+      variants={transitionVariants} 
+      initial='initial' 
+      animate='animate' exit='exit'
+      transition={{delay:0.4, duration:0.4, ease:'easeInOut' }}
+      >
+      </motion.div>
     </>
-  );
-};
+  )
+}
 
-export default Transition;
+export default Transition
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
 import '../styles/globals.css'
-import Layout from '../components/Layout'
+import Layout from '../components/Layout' //Components-layout
+import Transition from '../components/Transition' // Components Transitions
+import {useRouter} from 'next/router'   // router
+import {AnimatePresence, motion} from 'framer-motion'   // framer-motion
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
   
   <Layout>
-    <Component {...pageProps} />
+    <AnimatePresence mode='wait'>  
+      <motion.div key={router.route} className='h-full'>
+        <Transition/>
+        <Component {...pageProps} />
+      </motion.div>
+    </AnimatePresence>
 
   </Layout> 
 )}
